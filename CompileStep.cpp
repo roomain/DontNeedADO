ef41893bd38505d91fe601b72df17f04ddbf3522
romain <romain.crespel@gmail.com>
2022-08-25 16:39:34 +0200

    compiler

--- CompileStep.cpp
+++ CompileStep.cpp
@@ -60,11 +60,11 @@ QString CompileStep::getCompilerPath()const noexcept
 	switch (m_compiler)
 	{
 	case COMPILER::VS_2017:
-		sRet = "\"C:/Program Files (x86)/Microsoft Visual Studio/2017/Professional/MSBuild/Current/Bin/msbuild.exe\"";
+		sRet = "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/MSBuild/Current/Bin/msbuild.exe\"";
 		break;
 
 	case COMPILER::VS_2015:
-		sRet = "\"C:/Program Files (x86)/Microsoft Visual Studio/2015/Professional/MSBuild/Current/Bin/msbuild.exe\"";
+		sRet = "\"C:/Program Files (x86)/Microsoft Visual Studio/2019/Professional/MSBuild/Current/Bin/msbuild.exe\"";
 		break;
 
 	case COMPILER::VS_2019:
